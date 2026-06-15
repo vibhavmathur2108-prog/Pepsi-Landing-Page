a2d1700f23c103aa87db09c4ec8fa55392c19e47
vibhavmathur2108-prog <vibhavmathur2108@gmail.com>
2026-06-15 15:17:22 +0530

    Delete Landing Page/index.js

--- Landing Page/index.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav_menu');
-
-hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
